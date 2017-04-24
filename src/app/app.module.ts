3a0231d5d3e2dcd6766956ca08a97e5a2d0828ce
Shakti P Patro <shakti.patro@poweradv-dev41.poweradvocate.local>
2017-04-24 18:08:49 -0400

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
-
-import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [
-  { path: 'search', component: SearchComponent },
-  { path: 'detail/:id', component: DetailComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-];
-
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SearchComponent } from './search/search.component';
 import { DetailComponent } from './detail/detail.component';
 import {RoboAssistantService} from "./service/roboAssistant.service";
 import {RoboReviewsService} from "./service/roboReviews.service";
+import { PageNotFoundComponent } from "./not-found.component";
+
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'detail/:id', component: DetailComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -25,6 +25,7 @@ import {RoboReviewsService} from "./service/roboReviews.service";
     DashboardComponent,
     SearchComponent,
     DetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
